37543d5061e526a831ec6d5cb0022641afe59f0b
Everson Coqueiro <91283739+ecockeiro@users.noreply.github.com>
2023-02-01 10:21:56 -0300

    Update recorta_ciclone.py

--- recorta_ciclone.py
+++ recorta_ciclone.py
@@ -8,36 +8,49 @@ Created on Fri Jan 27 09:43:04 2023
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import pandas as pd
-import pandas as pd
+import cartopy.feature as cfeature
+import numpy as np
 
-df = pd.read_csv('/home/everson/CENPES/dados_track/d_1705.csv')
+#dataset
+df = pd.read_csv('/work/archive/Everson/Coqueiro/CENPES/DADOS/Tracks_planilhas/d-12.csv', sep=',')
 
-dia_inicial = int(17)
-dia_final = int(20)
+#lat e lon extend da imagem
+latmin, latmax = -45, -20
+lonmin, lonmax = -60, -35
 
-df2 = pd.DataFrame()
-for linha in df.itertuples():
-	#print(linha.Data)
-	dia = linha.Data.split()[0]
-	if int(dia[-2:]) >= dia_inicial and int(dia[-2:]) <=dia_final:
-		#print(linha.split())
-		df2 = df2.append(pd.DataFrame([linha]), ignore_index=True)
-		#print(df2.head())
-		#break
         
-#df2 = df[(df.Longitude>=0) & (df.Latitude<=-29) & (df.Latitude>=-38)].copy()
-df2 = df[(df.Longitude>=308) & (df.Longitude<=342) & (df.Latitude<=-29) & (df.Latitude>=-38)].copy()
+#converte e recorta latitude/longitude
+df['Longitude'] = (((df['Longitude']+180) %360) -180)
+data = df[(df.Longitude>=-55) & (df.Longitude<=-40) & (df.Latitude<=-29) & (df.Latitude>=-38)].copy()
 
-df2.to_csv('d_1705_nova.csv')    
+#salva a planilha recortada
+data.to_csv('/work/archive/Everson/Coqueiro/CENPES/DADOS/Track_recortados/d-12.csv')    
 
+#plota os tracks
+fig = plt.figure(figsize=(10,5))
+args = dict(color='gray',
+            alpha=1.0, 
+            linestyle='--', 
+            linewidth=0.5,
+            xlocs=np.arange(lonmin, lonmax, 10), 
+            ylocs=np.arange(latmin, latmax, 10), 
+            draw_labels=True)
 
-
-data = pd.read_csv('d_1705_nova.csv')
-ax = plt.axes(projection=ccrs.Robinson())
-ax.coastlines()
+ax = plt.axes(projection=ccrs.PlateCarree())
+gl = ax.gridlines(crs=ccrs.PlateCarree(), **args)
+gl.xlabel_style = {'size': 15, 'color': 'black', 'rotation': 0}
+gl.ylabel_style = {'size': 15, 'color': 'Gray', 'rotation': 0}
+gl.top_labels = False
+gl.right_labels = False
+gl.bottom_labels = True
+gl.rotate_labels = True
+ax.set_extent([lonmin, lonmax, latmin, latmax], ccrs.PlateCarree())
+ax.add_feature(cfeature.LAND)
+ax.coastlines(resolution='50m', color='black', linewidth=1)
 
 #one_ciclone = data[(data.N_do_Ciclone == 200)].copy()
 def plot_cyclones(ciclone):
+
     for i in range(len(ciclone)-1):
         #print(range(len(ciclone)))
         #print(i)
@@ -73,4 +86,7 @@ ciclones = list( dict.fromkeys(data.N_do_Ciclone) )
 for Ciclone in ciclones:
     one_ciclone = data[(data.N_do_Ciclone == Ciclone)].copy()
     plot_cyclones(one_ciclone)
+    
+    
+    
 plt.show()
