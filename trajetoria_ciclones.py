d310727c79beeb17be83e6226dec5e00f30bba02
Everson Coqueiro <91283739+ecockeiro@users.noreply.github.com>
2023-02-02 10:38:04 -0300

    Update and rename trajetoria_cartopy.py to trajetoria_ciclones.py

--- trajetoria_ciclones.py
+++ trajetoria_ciclones.py
@@ -16,8 +16,8 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import cartopy.io.shapereader as shpreader # Import shapefiles
 
-lonmin, lonmax = -55, -30
-latmin, latmax = -40, -15
+lonmin, lonmax = -55, -40
+latmin, latmax = -40, -25
 
 # fig = plt.figure(figsize=(10,6))
 # ax = fig.add_subplot(111)
@@ -25,24 +25,23 @@ latmin, latmax = -40, -15
 fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
 
-args = dict(color='gray',
-            alpha=1.0, 
-            linestyle='--', 
-            linewidth=0.5,
-            xlocs=np.arange(-180, 180, 30), 
-            ylocs=np.arange(-90, -45, 10), 
-            draw_labels=True)
+args = dict(draw_labels=True,
+            linewidth=1, color='gray', 
+            alpha=0.5, linestyle='--', 
+            xlocs=np.arange(-180, 180, 3), 
+            ylocs=np.arange(-50, 0, 3))
+
 
 gl = ax.gridlines(crs=ccrs.PlateCarree(), **args)
 gl.xlabel_style = {'size': 15, 'color': 'black', 'rotation': 0}
-gl.ylabel_style = {'size': 15, 'color': 'Gray', 'rotation': 0}
-gl.top_labels = True
-gl.right_labels = True
+gl.ylabel_style = {'size': 15, 'color': 'black', 'rotation': 0}
+gl.top_labels = False
+gl.right_labels = False
 gl.bottom_labels = True
 gl.rotate_labels = True
 ax.set_extent([lonmin, lonmax, latmin, latmax], ccrs.PlateCarree())
 ax.add_feature(cfeature.LAND)
-ax.add_feature(cfeature.OCEAN)
+# ax.add_feature(cfeature.OCEAN)
 ax.coastlines(resolution='50m', color='black', linewidth=1)
 
 shapefile = list(
@@ -57,18 +56,7 @@ ax.add_geometries(
     linewidth=0.5
     )
 
-# m = Basemap(projection='merc', resolution="f", llcrnrlon=lonmin, llcrnrlat=latmin, urcrnrlon=lonmax, urcrnrlat=latmax)
 
-# m.drawcoastlines(color='k',linewidth=0.5,zorder=3)
-# m.drawcountries(color='k',linewidth=0.1,zorder=3)
-
-# parallelmin = int(latmin)
-# parallelmax = int(latmax)+1
-# m.drawparallels(np.arange(parallelmin, parallelmax,5,dtype='int16').tolist(),labels=[1,0,0,0],linewidth=0,fontsize=10, zorder=3)
-
-# meridianmin = int(lonmin)
-# meridianmax = int(lonmax)+1
-# m.drawmeridians(np.arange(meridianmin, meridianmax,5,dtype='int16').tolist(),labels=[0,0,0,1],linewidth=0,fontsize=10, zorder=3)
 
 colors = ['C0','C1','C3','C2','C4','C5','C6','C7','C8','C9','C10','C11','C12','C13'] #default colors from Python (can be automated if the order is not important)
 datafiles = glob.glob("/work/archive/Everson/Coqueiro/CENPES/DADOS/Track_recortados/*.csv") #to read individual data files containing the coordinates of the track for each typhoon
@@ -85,17 +73,19 @@ for jj in range(6):
     dff['Latitude'] = round(dff.Latitude / 0.25) * 0.25
     dff['Longitude'] = round(dff.Longitude / 0.25) * 0.25
 
-    rodada = datafiles[0].split('/')[5].split('.')[0] #extract rodada information from the filename
-    track_name = datafiles[0].split('/')[5].split('.')[0]#extract track information from the filename
+    rodada = datafiles[jj].split('/')[8].split('.')[0] #extract rodada information from the filename
+    track_name = datafiles[0].split('/')[8].split('.')[0]#extract track information from the filename
     track_name = track_name.capitalize()
     
     
     lons = dff['Longitude'].values
     lats = dff['Latitude'].values
-    # x, y = ax(lons, lats)
+   
+    ax.plot(lons, lats,color=colors[jj],ms=4, zorder=4, label=f"TRACK {jj} ({rodada})")
+    ax.set_title("Trajetória do Yakecan - GFS 0.25", 
+                 fontweight='bold', 
+                 fontsize=20,)
+    ax.legend(loc='lower left')
     
-
-    # ## plot the track
-    ax.plot(lons, lats,color=colors[jj],ms=4, zorder=4,  label=f"TRACK {jj} ({rodada})", )
-    
+    plt.savefig('/work/archive/Everson/Coqueiro/CENPES/Imagens/trajetória_yakecan.png', bbox_inches='tight')
     
